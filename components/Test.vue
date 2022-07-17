ce17ad4369647481b63d17023f0360d965c3be78
Chen Frederick <frederickfd88@gmail.com>
2022-07-17 16:21:25 +0700

    Added Components

---
+++ components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>Test here - {{ text }}</div>
+    <div>{{isHiText}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      text: "super text",
+    };
+  },
+  computed: {
+    isHiText() {
+      return this.text === 'hi'
+    }
+  },
+  mounted() {
+    this.text = "hi";
+  },
+};
+</script>
+
+<style></style>
