eec48e750d22ff04bd4769be32521be0eea1af4e
Chen Frederick <frederickfd88@gmail.com>
2022-07-17 16:21:41 +0700

    Added Pages

---
+++ pages/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <Test />
+</template>
+
+<script>
+import Test from "../components/Test.vue";
+
+export default {
+  name: "TestPage",
+  components: {
+    Test,
+  },
+};
+</script>
